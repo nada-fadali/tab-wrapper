688381c9ae27f231237cff7bc6d00248c475689c
Nada Fadali <nada.fadali@gmail.com>
2015-11-07 19:39:29 +0200

    iframes dimensions random

--- wrap.js
+++ wrap.js
@@ -3,12 +3,20 @@ var urls;
 $(window).load(function() {
 	// get opened tabs url
   	chrome.runtime.onMessage.addListener(function(request, sender) {
-	
+
 		if (request.action == "wrap") {
 			urls = request.urls;
 
+			var html, width, height, rand;
+
 			for (var i = 0; i < urls.length-1; i++) {
-				var html = '<iframe src="' + urls[i] + '" width="500" height="600"></iframe>';
+				rand = Math.random();
+				while(rand==0) rand = Math.random();
+
+				width = window.innerWidth - (window.innerWidth * rand);
+				height = window.innerHeight - (window.innerHeight * rand);
+				
+				html = '<iframe src="' + urls[i] + '" width="' + width + '" height="' + height + '"></iframe>';
 
 				$("#wrapper").append(html);
 			};
